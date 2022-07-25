4431d96a916bb00fbc0ef51f24c964b0f3b4afd9
DingLifenote <109845010+DingLifenote@users.noreply.github.com>
2022-07-25 23:58:50 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -14,10 +14,11 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-# get channel_secret and channel_access_token from your environment variable
+# 從設定好的環境變數，取得LINE要用的的channel_secret和channel_access_token
 channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
 channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
 
+# 確定環境變數有設定好，才允許執行程式
 if channel_secret is None:
     print('Specify LINE_CHANNEL_SECRET as environment variable.')
     sys.exit(1)
@@ -29,7 +30,7 @@ if channel_access_token is None:
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
-
+# 宣告callback路由，連到LINE的Channel裡面的Webhook
 @app.route("/callback", methods=['POST'])
 def callback():
     # get X-Line-Signature header value
@@ -51,8 +52,10 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    # 收到訊息後，自動回傳訊息
     line_bot_api.reply_message(
         event.reply_token,
+        # 回傳訊息的格式為 "自動發言："加上使用者原本的訊息
         TextSendMessage(text="自動發言：" + event.message.text))
 
 
